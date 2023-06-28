376ed4867677d241a8b13fe027ac5527616df63a
Jan-Hendrik <jan-hendrik.peter@web.de>
2023-06-28 23:02:22 +0200

    added "Hours" to shift model

--- src/models/shift_model.go
+++ src/models/shift_model.go
@@ -8,10 +8,11 @@ import (
 
 type Shift struct {
 	gorm.Model
-	UserID uint `json:"userId" form:"userid" gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;default null"`
+	UserID    uint      `json:"userId" form:"userid" gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;default null"`
 	StartTime time.Time `json:"startTime" form:"startTime" gorm:"default:current_timestamp"`
-	EndTime time.Time `json:"endTime" form:"endTime" gorm:"default:current_timestamp"`
-	Comment string `json:"comment" form:"comment" gorm:"text"`
-	Rate float64 `json:"rate" form:"rate" gorm:"not null"`
-	StatusID uint `json:"statusId" form:"statusID" gorm:"foreignKey:StatusID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	EndTime   time.Time `json:"endTime" form:"endTime" gorm:"default:current_timestamp"`
+	Hurs      float64   `json:"hours" gorm:"not null;defualt null"`
+	Comment   string    `json:"comment" form:"comment" gorm:"text"`
+	Rate      float64   `json:"rate" form:"rate" gorm:"not null"`
+	StatusID  uint      `json:"statusId" form:"statusID" gorm:"foreignKey:StatusID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
